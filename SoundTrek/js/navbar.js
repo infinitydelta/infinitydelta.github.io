2b624fa5334626fa86d109f1fc7d1f72601a1085
infinitydelta <kylesan123@gmail.com>
2015-03-02 00:38:24 -0800

    a lot of updates
    - about me updated
    - add polygon apocalypse
    - fixed more lower resolution
    - soundtrek test

---
+++ SoundTrek/js/navbar.js
@@ -0,0 +1,6 @@
+function toggleDropdown() {
+    if (document.getElementById('navbar_main').style.height != '225px')
+        document.getElementById('navbar_main').style.height = '225px';
+    else
+        document.getElementById('navbar_main').style.height = '20px';
+}
