e4c1b960e5c8084fdfcecca59eb6ae18938c0224
infinitydelta <kylesan123@gmail.com>
2015-03-02 01:48:08 -0800

    more content update
    - iframe soundtrek from aarons site
    - added ultimate space combat
    - added sisyphus
    - added you were born to do this link to global game jam site

--- SoundTrek/js/navbar.js
+++
@@ -1,6 +0,0 @@
-function toggleDropdown() {
-    if (document.getElementById('navbar_main').style.height != '225px')
-        document.getElementById('navbar_main').style.height = '225px';
-    else
-        document.getElementById('navbar_main').style.height = '20px';
-}
